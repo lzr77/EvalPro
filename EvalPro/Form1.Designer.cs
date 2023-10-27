d8ca76263778feb0a3df90cb02c5f4110cc55674
Luca Zauner <luca.zauner@vector.com>
2023-10-27 12:40:10 +0200

    Titel verändern

--- EvalPro/Form1.Designer.cs
+++ EvalPro/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "EvalPro";
+            this.Text = "Anmeldung";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
